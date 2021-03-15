9b4fdcedfc89b71827a61681b657873fb76e7af4
V <v@localhost.localdomain>
2021-03-15 10:20:05 -0600

    finished 15 march 2021

--- sniffer.c
+++ sniffer.c
@@ -13,172 +13,125 @@
 #define MAXBUF 65536
 
 FILE *logs;
-FILE *macA;
 int size = 0;
 struct sockaddr_in source, dest;
 unsigned char buffer[MAXBUF];
 char netC[10];
-int ethII = 0, frameLoad = 0, framesTotal = 0;;
-int protocolNxLayer[6]={0}; // 0 -> ARP 1 -> IPV6 2 -> IPV4 3 -> CONTROL DE FLUJO 4 -> MAC Sec.
-int addrType = 0; // 1 -> unidifusion 3 -> difusion 2 -> multidifusion
-int isMAC = 0;
+int ethII = 0, frameLoad = 0, framesTotal = 0;
+int protocolNxLayer[6] = {0}; // 0 -> ARP 1 -> IPV6 2 -> IPV4 3 -> CONTROL DE FLUJO 4 -> MAC Sec.
+char direccionDest[18];
+char direccionOrig[18];
+uint16_t protocolo;
 
+typedef struct _Nodo {
+	char addMAC[18];
+	int cantidad;
+	struct _Nodo* sig;
+}Nodo;
 
-void typeOfAddr() {
-	switch(addrType) {
-		case 1:
-			fprintf(logs," -> Direccion de unidifusion. \n");
-			break;
-		case 2:
-			fprintf(logs," -> Direccion de multidifusion \n");
-			break;
-		case 3:
-			fprintf(logs," -> Direccion de difusion \n");
-			break;
-		default:
-			fprintf(logs," -> No identificado \n");
-			break;
+Nodo *direcc = NULL;
+
+Nodo *memoria(char addMAC[18]) {
+	Nodo *nuevo;
+	nuevo = (Nodo*)malloc(sizeof(Nodo));
+	nuevo->cantidad = 0;
+	nuevo->cantidad++;
+	strcpy(nuevo->addMAC,addMAC);
+	nuevo->sig = NULL;
+	return nuevo;
+}
+
+int macExists(Nodo * inicio, char addMAC[18]) {
+	Nodo * aux;
+	aux = inicio;
+	while(aux!=NULL) {
+		if(strcmp(aux->addMAC,addMAC)==0) {
+			return 1;
+		} else {
+			aux = aux->sig;
+		}
+	}
+	return 0;
+}
+
+Nodo *alta_inicio(Nodo *inicio, char addMAC[18]) {
+	Nodo *nuevo;
+	Nodo *aux;
+	int isMAC;
+	isMAC = macExists(inicio,addMAC);
+	if(isMAC == 1) {
+		aux = inicio;
+		while(aux!=NULL){
+			if(strcmp(aux->addMAC,addMAC) == 0) {
+				aux->cantidad++;
+			}
+			aux = aux->sig;
+		}
+	} else {
+		if(inicio == NULL) {
+		inicio = memoria(addMAC);
+		return inicio;
+		} else {
+			nuevo = memoria(addMAC);
+			nuevo->sig = inicio;
+			inicio = nuevo;
+		}	
+	}
+	return inicio;
+}
+
+void mostrar(Nodo *inicio) {
+	Nodo *aux;
+	aux = inicio;
+	fprintf(logs,"\n ----- MAC SUMMARY ----- \n");
+	while(aux!=NULL) {
+		fprintf(logs,"\n %s repetido %d veces. ", aux->addMAC, aux->cantidad);
+		aux = aux->sig;
 	}
 }
 
-void HextoBin(unsigned char tByt[2]) {
-	unsigned char symbol;
-	int j;
-	int bits[4];
-	for(j = 0; j < 2; j++) {
-		symbol = tByt[j];
-		if(symbol == '1')
-			bits[3] = 1;
-		if(symbol == '2')
-			bits[2] = 1;
-		if(symbol == '3')
-		{
-			bits[2] = 1;
-			bits[3] = 1;
-		}
-		if(symbol == '4')
-			bits[1] = 1;
-		if(symbol == '5')
-		{
-			bits[1] = 1;
-			bits[3] = 1;
-		}
-		if(symbol == '6')
-		{
-			bits[1]=1;
-			bits[2] = 1;
-		}
-		if(symbol == '7'){
-			bits[1] = 1;
-			bits[2] = 1;
-			bits[3] = 1;
-		}
-		if(symbol == '8')
-			bits[0] = 1;
-		if(symbol == '9'){
-			bits[0] = 1;
-			bits[3] = 1;
-		}
-		if(symbol == 'A') {
-			bits[0] = 1;
-			bits[2] = 1;
-		}
-		if(symbol == 'B')
-		{
-			bits[0] = 1;
-			bits[2] = 1;
-			bits[3] = 1;
-		}
-		if(symbol == 'C'){
-			bits[0]=1;
-			bits[1] = 1;
-		}
-		if(symbol == 'D'){
-			bits[0] = 1;
-			bits[1] = 1;
-			bits[3] = 1;
-		}
-		if(symbol == 'E'){
-			bits[0] = 1;
-			bits[1] = 1;
-			bits[2] = 1;
-		}
-		if(symbol == 'F') {
-			bits[0] = 1;
-			bits[1] = 1;
-			bits[2] = 1;
-			bits[3] = 1;
-		}
-	}
-	if(bits[3] == 1) {
-		addrType = 2;
+void HextoBin(char tByt[18]) {
+	if(tByt[0] == 'F' && tByt[1] == 'F') {
+		fprintf(logs," -> Direccion de difusion. \n");
 	} else {
-		addrType = 1;
+	   if(tByt[1] == '1' || tByt[1] == '3' || tByt[1] == '5' || tByt[1] == '7' || tByt[1] == '9' || tByt[1] == 'B' || tByt[1] == 'D' || tByt[1] == 'F') {
+			fprintf(logs," -> Direccion de multidifusion. \n");
+		} else {
+			fprintf(logs," -> Direccion de unidifusion. \n");
+		}
 	}
 }
 
 void ProtocolType (int typeOf){
 	switch(typeOf){
 			case 1544:
-				fprintf(logs,"**ARP**\n");
+				fprintf(logs,"******ARP******\n");
 				protocolNxLayer[0]++;
-				break;
+				break;	
 				
 			case 8:
-				fprintf(logs,"**IPv4**");
+				fprintf(logs,"******IPv4******");
 				protocolNxLayer[1]++;
 				break;
 				
 			case 56710:
-				fprintf(logs,"**IPv6**");
+				fprintf(logs,"******IPv6*******");
 				protocolNxLayer[2]++;
 				break;
-			
+				
 			case 2184:
-				fprintf(logs,"**Control de flujo Ethernet**");
+				fprintf(logs,"******Control de flujo Ethernet*****");
 				protocolNxLayer[3]++;
 				break;
 			
 			case 58760:
-				fprintf(logs,"**Seguridad MAC**");
+				fprintf(logs,"*******Seguridad MAC******");
 				protocolNxLayer[4]++;
 				break;
 			default: 
-				fprintf(logs,"**No identificado**");
+				fprintf(logs,"********No identificado********");
 				protocolNxLayer[5]++;
-	}
-}
-
-void PrintInHex(char *mesg, unsigned char *p, int len)
-{
-	fprintf(logs,"%s",mesg);
-	len--;
-	unsigned char dif[2] = "FF";
-	int i = 1;
-	int j = 0;
-	int h;
-	while(len)
-	{
-		if(*p == *dif && i < 3)
-		{
-			addrType++; //difusion
-			i = 3;
-		} else
-		{
-			if(i < 3)
-			{
-				if(i==2){
-					HextoBin(p);
-				}
-				i++;
-			}
-		}
-		fprintf(logs,"%.2X ", *p);
-		if(isMAC == 1){
-			fprintf(macA,"%.2X ", *p);
-		}
-		p++;
-		len--;
+				break;
 	}
 }
 
@@ -189,23 +142,22 @@ void ParseEthernetHeader(unsigned char *packet, int len)
 	{
 		fprintf(logs,"---- Frame: %d ---- \n", framesTotal);
 		ethII++;
-		fprintf(logs,"Trama Ethernet II. \n");
+		fprintf(logs,"***** Trama Ethernet II. *****");
 		ethernet_header = (struct ethhdr *)packet;
 		
-		isMAC = 1;
-		PrintInHex("MAC de destino: ", ethernet_header->h_dest, 6);
-		fprintf(logs,"\n");
-		fprintf(macA,"\n");
-		typeOfAddr();
+		sprintf(direccionDest,"%02x:%02x:%02x:%02x:%02x:%02x", ethernet_header->h_dest[0],ethernet_header->h_dest[1],ethernet_header->h_dest[2], ethernet_header->h_dest[3], ethernet_header->h_dest[4], ethernet_header->h_dest[5]);
+		fprintf(logs,"\n Direccion MAC destino: %s \n",direccionDest);
+		HextoBin(direccionDest);
+		direcc = alta_inicio(direcc,direccionDest);
 		
-		PrintInHex("MAC de la fuente: ", ethernet_header->h_source, 6);
-		fprintf(logs,"\n");
-		fprintf(macA,"\n");
-		typeOfAddr();
+		sprintf(direccionOrig,"%02x:%02x:%02x:%02x:%02x:%02x", ethernet_header->h_source[0],ethernet_header->h_source[1],ethernet_header->h_source[2], ethernet_header->h_source[3], ethernet_header->h_source[4], ethernet_header->h_source[5]);
+		fprintf(logs,"Direccion MAC origen; %s \n",direccionOrig);
+		HextoBin(direccionOrig);
+		direcc = alta_inicio(direcc,direccionOrig);
 		
-		isMAC = 0;
 		ProtocolType(ethernet_header->h_proto);
-		PrintInHex("\t Protocolo empaquetado: ",(void *)&ethernet_header->h_proto, 2);
+		protocolo = htons(ethernet_header->h_proto);
+		fprintf(logs,"\nProtocolo: 0x%04X ", protocolo);
 		fprintf(logs,"\n");
 
 		frameLoad = len - 18;
@@ -221,7 +173,6 @@ void ParseEthernetHeader(unsigned char *packet, int len)
 
 void *capturador(void *args){
     logs = fopen("sniffer.txt","a+");
-    macA = fopen("direccionesMAC.txt","a+");
     if(logs==NULL) {
 	printf("\n Error al abrir el archivo. ");
     }
@@ -231,9 +182,8 @@ void *capturador(void *args){
 
 void *analizador(void *args){
 	int packet = 0;
-	int packet_size;
+	int packet_size, saddr_size;
 	int i = 0;
-	int saddr_size;
 	struct sockaddr_in source_socket_address, dest_socket_address;
 	struct sockaddr saddr;
 
@@ -244,11 +194,11 @@ void *analizador(void *args){
 	scanf("%s",netC);
 
 	int s = socket (PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-	 if(s == -1)
-	 {
+	if(s == -1)
+	{
 		perror("Error en socket");
 		exit(1);
-	 }
+	}
 
 	struct ifreq ethreq;
 	strncpy (ethreq.ifr_name, netC, IFNAMSIZ);
@@ -257,9 +207,9 @@ void *analizador(void *args){
 	ioctl(s, SIOCSIFFLAGS, &ethreq);
 	
 	printf("\n Analizando paquetes... \n");
-	while(i<=packet) {
+	while(i<packet) {
 		saddr_size = sizeof saddr;
-		size = recvfrom(s , buffer , MAXBUF, 0 , &saddr , &saddr_size);
+		size = recvfrom(s, buffer, MAXBUF, 0 , &saddr , &saddr_size);
       		if (packet_size == -1) {
         		printf("NO se pudieron procesar los paquetes. \n");
         		exit(1);
@@ -283,7 +233,7 @@ int main() {
 	char command[50];
 	snprintf(command,sizeof(command),"/sbin/ifconfig %s -promisc",netC);
 	system(command);
-	printf("\n Haciendo recuentos... \n");
+	mostrar(direcc);
 	printf("\n Analisis terminado. \n Registros en: sniffer.txt \n");
 	return 0;
 }
